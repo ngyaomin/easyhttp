5a8a0d05d2485d1f28ca83afdc3f6ead55673056
Yao Min <ngyaominsg@gmail.com>
2018-05-15 18:45:18 +0800

    Add get single post

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = new easyHTTP;
 
 // Get posts
-http.get('https://jsonplaceholder.typicode.com/posts', 
-function(err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', 
+// function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts );
+//   }
+// });
+
+// get single post
+
+http.get('https://jsonplaceholder.typicode.com/posts/1', 
+function(err, post) {
   if(err) {
     console.log(err);
   } else {
-    console.log(posts );
+    console.log(post);
   }
 });

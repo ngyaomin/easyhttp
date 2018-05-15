9e288a85a1c61cf14cc60acc5ee610d7060c5c8f
Yao Min <ngyaominsg@gmail.com>
2018-05-15 15:48:52 +0800

    Get Request part 1

--- easyhttp.js
+++ easyhttp.js
@@ -2,3 +2,22 @@ function easyHTTP() {
   this.http = new XMLHttpRequest(); 
 }
 
+// make an Http get request
+easyHTTP.prototype.get = function(url) {
+  this.http.open('GET', url, true);
+  
+  this.Http.onload = function() {
+    if(this.http.status === 200) {
+      console.log(this.http.responseText);
+    }
+  };
+  
+  this.http.send();
+}
+
+// make an Http post request
+
+
+// make an Http put request
+
+// make an Http del request
